d56c50cf6474808024af17e64f045aa531c836f9
turyng <turyng@proton.me>
2023-07-08 18:17:20 +0200

    Add validation to model Message

--- messenger_project/core_app/models.py
+++ messenger_project/core_app/models.py
@@ -1,13 +1,21 @@
-import shortuuid, shortuuidfield
 import uuid
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Message(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     body = models.TextField(max_length=5000)
-    destroyLiveAfterSeconds = models.IntegerField(blank=True, null=True)
-    password = models.CharField(max_length=256, blank=True)
-    destroyAfterDays = models.IntegerField()
+    destroyLiveAfterSeconds = models.IntegerField(blank=True, null=True,
+        validators=[
+            MinValueValidator(0, message="Value must be greater than or equal to 1."),
+            MaxValueValidator(60, message="Value must be less than or equal to 30.")
+        ])
+    password = models.CharField(max_length=256)
+    destroyAfterDays = models.IntegerField(
+        validators=[
+            MinValueValidator(1, message="Value must be greater than or equal to 1."),
+            MaxValueValidator(30, message="Value must be less than or equal to 30.")
+        ])
                             
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now = True)
